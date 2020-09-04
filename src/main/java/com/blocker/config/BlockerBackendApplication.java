b54641b06dfa925aa4a1439d46778ea6862d0771
정성오 <wnw1102@naver.com>
2020-09-04 16:52:01 +0900

    [config] 설정중

---
+++ src/main/java/com/blocker/config/BlockerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blocker.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockerBackendApplication.class, args);
+	}
+
+}
