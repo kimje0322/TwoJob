3acec31b253482483f54de24393fe53c7b3c585f
이준형 <ouosssssssa@naver.com>
2020-09-10 08:22:26 +0000

    Update CrowdFunding.sol

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -5,29 +5,29 @@ import "./Campaign.sol";
 contract CrowdFunding is Campaign{
     mapping(string => campaignStruct) campaigns;
 
-    /** modifier ë functionì ì¤í(íë)ì ì½ê² ë³ê²½ê°ë¥íëë¡íë©°, function ì¤íì ì¡°ê±´ì ì²´í¬ í ì ìë¤
-     * @param _uniqueCode í´ë¹ìº íì¸ì í´ìê°
+    /** modifier 는 function의 실행(행동)을 쉽게 변경가능하도록하며, function 실행의 조건을 체크 할수 있다
+     * @param _uniqueCode 해당캠페인의 해시값
      */
     modifier campaignNotClosed(string memory _uniqueCode) {require(!campaigns[_uniqueCode].closed); _;}
     
-    /** ê²ìê¸ ìì±ìë§ íì©
-     * @param _uniqueCode í´ë¹ìº íì¸ì í´ìê°  
+    /** 게시글 작성자만 허용
+     * @param _uniqueCode 해당캠페인의 해시값  
      */
     modifier campaignOwner(string memory _uniqueCode) {require(msg.sender == campaigns[_uniqueCode].creator); _;}
 
-    /** ë§ê°ì¼ì´ ì§ë¬ëì§ íì¸íë modifier
-     * @param _uniqueCode í´ë¹ìº íì¸ì í´ìê°
-     * nowë ëì´ì ì§ìíì§ ìê¸°ëë¬¸ì block.timestampë¥¼ ì¬ì©í¨ ëê°ê° ê°ì ìë¯¸
+    /** 마감일이 지났는지 확인하는 modifier
+     * @param _uniqueCode 해당캠페인의 해시값
+     * now는 더이상 지원하지 않기때문에 block.timestamp를 사용함 두개가 같은 의미
      */
     modifier becomeDuedate(string memory _uniqueCode) {require(block.timestamp >= campaigns[_uniqueCode].deadline);_;}
 
 
-    /**  createCampaignìëí event
-     *@param _uniqueCode ê²ìê¸ìëí í´ìê°
-     *@param _creator ê²ììì ì£¼ì
-     *@param _fundingGoal ëª©í ê¸ì¡
-     *@param _fundraiseAmount íì¬ ê¸ì¡
-     *@param _deadline ë§ê°ì¼
+    /**  createCampaign에대한 event
+     *@param _uniqueCode 게시글에대한 해시값
+     *@param _creator 게시자의 주소
+     *@param _fundingGoal 목표 금액
+     *@param _fundraiseAmount 현재 금액
+     *@param _deadline 마감일
      */
     event noticeCreateCampaign(
         string  _uniqueCode,
@@ -37,10 +37,10 @@ contract CrowdFunding is Campaign{
         uint _deadline
     );
 
-    /**í¬ì ê²ìê¸ ìì± 
-       ê²ìê¸ìëí í´ìê°, ëª©íê¸ì¡, ë§ê°ì¼ì´ íë¼ë¯¸í°ë¡ ëê²¨ì§
-       í¬ìë¥¼ ë°ë ì¬ëì ì£¼ì, ëª©íê¸ì¡, ëª¨ì¸ê¸ì¡, í¬ììë¤ì ê°ë³ í¬ìê¸ì¡ììí mapping
-       ê±°ëì ë§ê°ì¼, ë§ê°ì¼ì´ ì¢ë£ëìëì§ ìëì§
+    /**투자 게시글 생성 
+       게시글에대한 해시값, 목표금액, 마감일이 파라미터로 넘겨짐
+       투자를 받는 사람의 주소, 목표금액, 모인금액, 투자자들의 개별 투자금액을위한 mapping
+       거래의 마감일, 마감일이 종료되었는지 아닌지
      */
     
     
@@ -49,7 +49,7 @@ contract CrowdFunding is Campaign{
         campaigns[_uniqueCode].creator = msg.sender;
         campaigns[_uniqueCode].fundingGoal = _fundingGoal;
         campaigns[_uniqueCode].fundraiseAmount = 0;
-        //mappingì ì´ê¸°íìì¼ì£¼ê¸°ìí´ ê²ììì ì£¼ìë¥¼ ê°ì§ê³  ì´ê¸°íë¥¼í´ì¤ ì¬ì©íì§ë ìììì 
+        //mapping을 초기화시켜주기위해 게시자의 주소를 가지고 초기화를해줌 사용하지는 않을예정
         //campaigns[_uniqueCode].investRate[msg.sender] = 0;
         campaigns[_uniqueCode].deadline = _deadline;
         campaigns[_uniqueCode].closed = false;
@@ -58,13 +58,13 @@ contract CrowdFunding is Campaign{
         emit noticeCreateCampaign(tempCamp.uniqueCode, tempCamp.creator, tempCamp.fundingGoal, tempCamp.fundraiseAmount, tempCamp.deadline);
     }
 
-    /**í¹ì  ê²ìê¸ì í¬ìíê¸°
-     *@param _uniqueCode í¬ìíê³ ì¶ì ê²ìê¸ìëí í´ìê°
+    /**특정 게시글에 투자하기
+     *@param _uniqueCode 투자하고싶은 게시글에대한 해시값
      */
     function FundingCampign(string memory _uniqueCode) payable public {
         campaignStruct storage tempCamp = campaigns[_uniqueCode];
         
-        //ê²ìê¸ ìì±ìë í¬ìë¥¼ ë§ëë¤
+        //게시글 작성자는 투자를 막는다
         require(msg.sender != tempCamp.creator);
 
         tempCamp.investgators.push(msg.sender);
@@ -73,24 +73,24 @@ contract CrowdFunding is Campaign{
     }
 
     /**
-     * ë§ê°ì¼ì´ ëë©´ ëª©íê¸ì¡ì ëë¬ íìê²½ì° íë© ê¸ì¡ì ìì±ììê² ì¤ 
-     * @param _uniqueCode ê²ìê¸ìëí í´ìê°
+     * 마감일이 되면 목표금액에 도달 했을경우 펀딩 금액을 작성자에게 줌 
+     * @param _uniqueCode 게시글에대한 해시값
      */
     function DueDay(string memory _uniqueCode) campaignNotClosed(_uniqueCode) campaignOwner(_uniqueCode) becomeDuedate(_uniqueCode) public {
         campaignStruct storage tempCamp = campaigns[_uniqueCode];
 
         if(tempCamp.fundingGoal > tempCamp.fundraiseAmount){
-            // í¬ììë¤ìê² í¬ìí ê¸ì¡ ë§í¼ ëì´ ì¡ê¸ë¨
+            // 투자자들에게 투자한 금액 만큼 돈이 송금됨
             uint256 len = tempCamp.investgators.length;
             uint count = 0;
             for(uint i=0; i<len; i++){
                 address invastgator = tempCamp.investgators[count++];
-                //0.5.0ë¶í°ë addressìì trasferë¥¼ í  ì ìê³  ëë²ì addressë¥¼ uint160ì¼ë¡ íë³í íë¤ì ë¤ì addressë¡ íë³í í´ì£¼ì´ì¼ ì¡ê¸ì´ ê°ë¥í´ ì§
+                //0.5.0부터는 address에서 trasfer를 할 수 없고 두번의 address를 uint160으로 형변환 한뒤에 다시 address로 형변환 해주어야 송금이 가능해 짐
                 address(uint160(invastgator)).transfer(tempCamp.investRate[invastgator]);
             }
 
         }else{
-            // creatorìê² í¬ìê¸ì¡ì´ ì¡ê¸ë¨
+            // creator에게 투자금액이 송금됨
             msg.sender.transfer(tempCamp.fundraiseAmount);
         }
 
