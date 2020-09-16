28606fbbc0f4f52b4df504250062978ff9e66896
권남선 <namseon1120@naver.com>
2020-09-16 16:16:00 +0900

    wireframe completed resolves S03P22B102-101

--- frontend/src/views/invest/WriteInvest.vue
+++ frontend/src/views/invest/WriteInvest.vue
@@ -30,27 +30,57 @@
     <div>
       <!-- 투자 글쓰기 메뉴 -->
       <div class="writeMenuBar">
-        <v-tabs
-          v-model="tab"
-          background-color="deep-purple accent-4"
-          class="elevation-2"
-          dark
-          :prev-icon="prevIcon ? 'mdi-arrow-left-bold-box-outline' : undefined"
-          :next-icon="nextIcon ? 'mdi-arrow-right-bold-box-outline' : undefined"
-          :icons-and-text="icons"
-        >
-          <!-- <v-tabs-slider></v-tabs-slider> -->
-          <v-tab v-for="(item, i) in tabs" :key="i" :href="`#tab-${i}`">
-            {{ item }}
-          </v-tab>
+        <v-tabs v-model="tab" class="elevation-2" dark hide-slider>
+          <v-tab v-for="(item, i) in tabs" :key="i" :href="`#tab-${i}`" class="writeMenu">{{ item }}</v-tab>
           <!-- 투자오픈버튼 -->
-          <div>
-            <v-btn>투자 오픈</v-btn>
+          <div class="openbtn">
+            <v-btn color="rgb(22, 150, 245)" style="font-weight: 600">투자 프로젝트 오픈</v-btn>
           </div>
           <!-- 프로젝트 정보 창 -->
           <v-tab-item :value="'tab-0'">
             <v-card flat tile>
-              <v-card-text>프로젝트 정보</v-card-text>
+              <v-card-text>
+                <div class="pjtinfo">
+                  <p>프로젝트에대한 정보를 정확하게 입력해주세요.</p>
+                  <h5>프로젝트명</h5>
+                  <input type="text" placeholder="프로젝트명을 입력해주세요." />
+                  <h5>프로젝트 한줄 소개</h5>
+                  <input type="text" placeholder="프로젝트에대해 100자이내로 설명해주세요." />
+                  <h5>프로젝트 오픈 기간</h5>
+                  <div>
+                    <!-- 시작날짜 -->
+                    <div>
+                      <p>시작 날짜</p>
+                      <div>
+                        <v-menu
+                          ref="menu1"
+                          v-model="menu1"
+                          :close-on-content-click="false"
+                          transition="scale-transition"
+                          offset-y
+                          max-width="290px"
+                          min-width="290px"
+                        >
+                          <template v-slot:activator="{ on, attrs }">
+                            <v-text-field
+                              v-model="dateFormatted"
+                              persistent-hint
+                              v-bind="attrs"
+                              @blur="date = parseDate(dateFormatted)"
+                              v-on="on"
+                              color="rgb(22, 150, 245)"
+                              hide-details
+                              outlined
+                              placeholder="프로젝트 시작 날짜"
+                            ></v-text-field>
+                          </template>
+                          <v-date-picker v-model="date" no-title @input="menu1 = false" color="rgb(22, 150, 245)"></v-date-picker>
+                        </v-menu>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </v-card-text>
             </v-card>
           </v-tab-item>
           <!-- 금손 정보 창 -->
@@ -72,16 +102,44 @@
 </template>
 
 <script>
-import "@/../public/css/WriteInvest.css"
+import "@/../public/css/WriteInvest.css";
 
 export default {
   data() {
     return {
       tab: null,
-      text: ['1', '2', '3'],
-      tabs: ['프로젝트 정보', '금손 정보', '투자 설명서'],
-    }
-  }
+      text: ["1", "2", "3"],
+      tabs: ["프로젝트 정보", "금손 정보", "투자 설명서"],
+      // 날짜
+      date: "",
+      dateFormatted: "",
+      menu1: false,
+    };
+  },
+  computed: {
+    // computedDateFormatted () {
+    //   return this.formatDate(this.date)
+    // },
+  },
+  watch: {
+    date(val) {
+      this.dateFormatted = this.formatDate(this.date);
+    },
+  },
+  methods: {
+    formatDate(date) {
+      if (!date) return null;
+
+      const [year, month, day] = date.split("-");
+      return `${year}/${month}/${day}`;
+    },
+    parseDate(date) {
+      if (!date) return null;
+
+      const [year, month, day] = date.split("/");
+      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    },
+  },
 };
 </script>
 
@@ -91,6 +149,7 @@ export default {
   text-align: center;
   line-height: 50px;
   border-bottom: 1px solid gray;
+  margin-bottom: 15px;
 }
 .items div {
   display: inline-block;
@@ -106,7 +165,49 @@ export default {
 .items h5 {
   font-weight: 600;
 }
-.theme--dark.v-tabs > .v-tabs-bar {
-  background: unset;
+.writeMenu {
+  margin-left: 3%;
+  letter-spacing: unset !important;
+  font-size: 1.25rem;
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+}
+.writeMenu:hover {
+  background-color: rgba(173, 220, 254, 0.4);
+}
+.v-tab--active {
+  background-color: rgba(173, 220, 254, 0.4);
+}
+.v-tab:before {
+  background-color: unset;
+}
+.openbtn {
+  line-height: 45px;
+  position: absolute;
+  right: 1%;
+}
+.v-card__text {
+  padding: 16px 10%;
+  color: black !important;
+}
+.pjtinfo h5 {
+  font-size: 1.15rem;
+  font-weight: 600;
+}
+.pjtinfo input {
+  background-color: white;
+  width: 90%;
+  height: 40px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  margin-left: 10px;
+  margin-bottom: 30px;
+  padding: 10px;
+}
+.v-input__slot fieldset {
+  display: none;
+}
+.v-menu {
+  display: unset;
 }
 </style>
