256ef49daa607ed6e290e1f20c7cab2cb04a5405
이준형 <ouosssssssa@naver.com>
2020-09-21 06:46:02 +0000

    Update ISaleItem.sol

--- Smartcontract/ISaleItem.sol
+++ Smartcontract/ISaleItem.sol
@@ -4,11 +4,13 @@ interface ISaleItem{
     
    /** 물건 판매
     @param _uniqueCode 게시글에 대한 해시값
+    @param _count 판매갯수
     */
     function SaleItem(string memory _uniqueCode, uint count) payable external;
 
     /** 수익 분배
     @param _uniqueCode 게시글에 대한 해시값
+    @param _money 투자자들에게 나눠줄 수익금
     */
     function DistributeProfit(string memory _uniqueCode, uint256 money) payable external;
 }
