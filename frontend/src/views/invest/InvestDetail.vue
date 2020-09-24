e867d9e5e4c6b36ec3079e356fc41d0b79ef3210
권남선 <namseon1120@naver.com>
2020-09-24 16:28:14 +0900

    [FE] Add investdetail design

---
+++ frontend/src/views/invest/InvestDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="investdetail">
+    <navbar/>
+    <!-- 상품 게시글 제목 -->
+    <div class="investTitle" style="height: 230px; background-color: lightgrey;">
+      <h4 style="text-align: center; padding-top:95px;">Ostay Hair dryer Dock for Dyson</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "../../components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar
+  },
+}
+</script>
+
+<style>
+
+</style>
