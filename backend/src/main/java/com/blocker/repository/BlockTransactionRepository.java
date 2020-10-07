2fadebfb07e0692d89fbf1f1adce370a4591c381
정성오 <wnw1102@naver.com>
2020-10-07 17:45:07 +0900

    [BC] WHY?

--- backend/src/main/java/com/blocker/repository/BlockTransactionRepository.java
+++ backend/src/main/java/com/blocker/repository/BlockTransactionRepository.java
@@ -15,7 +15,7 @@ public interface BlockTransactionRepository extends JpaRepository<BlockTransacti
 	List<BlockTransaction> findByToaddressAndType(String address,TransactType type);
 	Page<BlockTransaction> findByFromaddressAndTypeflag(String address, int typeflag,Pageable pageable);
 	List<BlockTransaction> findByFromaddressAndType(String address,TransactType type,Pageable pageable);
-	
+	Page<BlockTransaction> findByFromaddress(String address,Pageable pageable);
 	@Query(value = "SELECT DISTINCT(p.fromaddress) FROM blocktransact p WHERE p.toaddress = ?1 and p.type = ?2", nativeQuery = true)
 	List<String> findthis(String address, String type);
 }
